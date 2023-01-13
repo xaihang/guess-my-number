77b7a6adcaf0b02ac9b3ed753a9d37f0a5d9d542
Xai Hang <xaihang12@gmail.com>
2023-01-12 19:35:53 -0600

    added global secret number

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+//game logics:
+//      if the guess is correct (=== secret number)
+//       or guess is too hight to secret number
+//       or guess is too low to secret number
+
+//Secret number needs to be global, so set it outside of the .check button/addEventListener
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number; 
+
+
 //handling click on the 'check!' button: by using addEventListener() method
 document.querySelector('.check').addEventListener('click', function() {
     // log the number we enter in the square, will ONLY be call once 'check!' button is 'click'
@@ -32,4 +42,5 @@ document.querySelector('.check').addEventListener('click', function() {
     if(!guess) {
         document.querySelector('.message').textContent = '⛔️ No number!'; 
     }
-})
+});
+
