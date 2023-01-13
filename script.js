5a01a01eb6d17758cd946cfd94ec91da63156c47
Xai Hang <xaihang12@gmail.com>
2023-01-12 19:13:13 -0600

    added correct num msg

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+//What is DOM manipulation? 
+//      Document Object Model: a connection point between HTML doc and JS 
+
+//logging message to the console to see: 
+console.log(document.querySelector('.message').
+textContent); 
+
+//correct number message:
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+//secret number: 
+
